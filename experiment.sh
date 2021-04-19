19fbb5a5a7c0bdf3449a7a2ad3f6ee3d0d5b3b15
derosejf <joeyfdr@yahoo.com>
2021-04-19 00:15:10 -0400

    generic experiment script

---
+++ experiment.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+seed=$1
+
+output_dir=/content/gdrive/MyDrive/Humor-Detection/"${json_file%.json}"_"$seed"
+echo "$output_dir"
+rm -rf "$output_dir"
+mkdir "$output_dir"
+echo Beginning training. Writing to "$output_dir"
+
+export BERT_BASE_DIR=./uncased_L-24_H-1024_A-16
+export CUDA_VISIBLE_DEVICES=0
+python3 run_classifier.py \
+--task_name=cola \
+--do_train \
+--do_eval \
+--data_dir=./data \
+--max_seq_length=128 \
+--train_batch_size=256 \
+--learning_rate=2e-5 \
+--num_train_epochs=1.0 \
+--do_lower_case \
+--gradient_accumulation_steps 8 \
+--overwrite_cache \
+--bert_model=bert-base-uncased \
+--output_dir=./final_results \
+--ambiguity_fn=csi \
+--seed "$seed"
+
